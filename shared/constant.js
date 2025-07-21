ac1b813ae2fc0dda21d07b56fcf3ddf62dd5647c
epicsven-cup <jackylin616@gmail.com>
2025-07-21 00:03:30 -0400

    remove js files

--- shared/constant.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULT = void 0;
-exports.DEFAULT = {
-    state: "In menu",
-    details: "Looking for Songs",
-    startTimestamp: Date.now(),
-};
